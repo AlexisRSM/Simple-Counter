260c998eb927c893839be8148a23cbeeddc6840f
AlexisRSM <alexismendes98@hotmail.com>
2024-06-10 16:40:01 +0100

    Test

--- src/SecondsCounter.jsx
+++ src/SecondsCounter.jsx
@@ -1,9 +1,22 @@
-function SecondsCounter() {
-    return ( 
-        <div className="Counter">
+import { useState, useEffect } from 'react';
 
+function SecondsCounter() {
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(time + 1);
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        }
+    }, [time]);
+    ;
+    return (
+        <div className="Counter">
+            Time {time}
         </div>
-     );
+    );
 }
 
 export default SecondsCounter;
